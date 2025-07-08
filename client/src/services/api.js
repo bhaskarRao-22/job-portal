2c6fafdf844f57491be11135d806d55bd77ccf3c
K Bhaskar Rao <kbhaskarrao022@example.com>
2025-07-08 13:19:05 +0530

    today

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+console.log("✅ BASE URL:", import.meta.env.VITE_API_BASE_URL);
 const API = axios.create({
   // baseURL: "http://localhost:5000/api",
   baseURL: import.meta.env.VITE_API_BASE_URL,
